eeb6869aeccde469db5ac2cfba08f7af7296b12f
Adel Setoodehnia <asetoodehnia@berkeley.edu>
2017-03-04 14:08:46 -0800

    Update PalindromeFinder.java

--- PalindromeFinder.java
+++ PalindromeFinder.java
@@ -5,7 +5,10 @@ public class PalindromeFinder {
 		load in and read through the words.txt file and 
 		print out all the words that are palindromes!
 		
+		if you can finish this then change it so that it takes
+		in a user input and checks to see if THAT is a palidrome!
 		*/
+		
     }
 
 }
