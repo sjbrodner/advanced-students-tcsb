a0bde31e422e01ba41e96044de62efe566db2da1
asetoodehnia <asetoodehnia@gmail.com>
2017-03-04 14:02:06 -0800

    added skeleton for PalindromeFinder

---
+++ PalindromeFinder.java
@@ -0,0 +1,11 @@
+public class PalindromeFinder {
+	public static void main(String[] args) {
+        /* Your code goes here!!
+		
+		load in and read through the words.txt file and 
+		print out all the words that are palindromes!
+		
+		*/
+    }
+
+}
