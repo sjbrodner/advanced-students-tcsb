b84507de3858324e7c208b409f1c738946baf7ae
Adel Setoodehnia <asetoodehnia@berkeley.edu>
2017-03-04 14:20:49 -0800

    Update PalindromeFinder.java

--- PalindromeFinder.java
+++ PalindromeFinder.java
@@ -2,10 +2,12 @@ public class PalindromeFinder {
 	public static void main(String[] args) {
         /* Your code goes here!!
 		
-		load in and read through the words.txt file and 
+		Load in and read through the words.txt file and 
 		print out all the words that are palindromes!
 		
-		if you can finish this then change it so that it takes
+		If the word IS a palindrome, then add it into a set!
+		
+		If you can finish this then change it so that it takes
 		in a user input and checks to see if THAT is a palidrome!
 		*/
 		
