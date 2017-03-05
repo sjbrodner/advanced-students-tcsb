794ce9d26097a9179604af90985416cb7e2cbac7
sjbrodner <simonebrodner@gmail.com>
2017-03-04 20:48:34 -0800

    Update PalindromeFinder.java

--- PalindromeFinder.java
+++ PalindromeFinder.java
@@ -1,15 +1,33 @@
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class PalindromeFinder {
 	public static void main(String[] args) {
-        /* Your code goes here!!
+		Set<String> s = new HashSet<String>();
+        	Palindrome p = new Palindrome();
+		try{ 
+			BufferedReader reader = new BufferedReader(new FileReader("words.txt"));
+			String line;
+			while((line = reader.readLine()) != null){
+				if(p.isPalindrome(line)){
+					s.add(line);
+					System.out.println(line);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		Scanner reader = new Scanner(System.in);
+		while(true){
+			System.out.println();
+			System.out.println("Please Enter a Word: ");
+			String test = reader.nextLine(); 
+			System.out.println("is palindrome: " + p.isPalindrome(test));
+			System.out.println("is in set: " + s.contains(test));
 		
-		Load in and read through the words.txt file and 
-		print out all the words that are palindromes!
-		
-		If the word IS a palindrome, then add it into a set!
-		
-		If you can finish this then change it so that it takes
-		in a user input and checks to see if THAT is a palidrome!
-		*/
 		
     }
 
