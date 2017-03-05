2e5f10917b62fbac8e0d2f295d65b8371e07afa2
sjbrodner <simonebrodner@gmail.com>
2017-03-04 20:46:06 -0800

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,10 +1,16 @@
-/** This class contains useful functions for testing palindromes.
+/** This class contains useful functions for testing palindromesc class Palindrome {
 */
 
 public class Palindrome {
 
-	public boolean isPalindrome(String word){
-		/** YOUR CODE GOES HERE */
-	}
-
+        public boolean isPalindrome(String word){
+                if(word.length() <=  1){
+                        return true;
+                }
+                for(int i = 0; i < (word.length()/2); i++){
+                        if(word.charAt(i) != word.charAt(word.length() - i - 1))
+                                return false;
+                }
+                return true;
+        }
 }
